4e7d538de52df937a904a2a52ce2f40f84ab57e6
WendPy <75748471+Hosjev@users.noreply.github.com>
2023-08-08 08:45:51 -0700

    Update cc_81.ts

--- Typescript/exercises/cc_81.ts
+++ Typescript/exercises/cc_81.ts
@@ -3,8 +3,6 @@ import axios from 'axios'
 let _token
 let _accessTokenExpire
 
-const perimeter81BaseUrl = 'https://api.perimeter81.com/api'
-const p81NetworkReadAPIKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhZmYzZDEyYjE3MmQ2MGJiNjI2ZjRjMDE5YjExM2U3YiIsImlkIjoiR2lVdFJQN1NkNyIsInNjb3BlcyI6WyJORVRXT1JLX1JFQUQiLCJORVRXT1JLX0hFQUxUSF9SRUFEIl0sInRlbmFudElkIjoiY2hpcHBlciIsImlhdCI6MTY2NzkzMDcwNiwiZXhwIjo0Nzg5OTk0NzA2LCJhdWQiOiJzYWZlci1hdXRoIiwiaXNzIjoiYXV0aC5zYWZlcnNvZnR3YXJlLmNvbSIsInN1YiI6ImNoaXBwZXIifQ.l68AC3c9lnbLIIJ3NSjBU-20ZBYIjh9aVEHIlVFptUA'
 
 const getPerimeterEightOneToken = async () => {
   if (_token && Date.now() - _accessTokenExpire < 3600) return _token
